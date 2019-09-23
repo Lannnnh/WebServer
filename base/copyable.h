9690062eb3ea55894ed2120877b47971e1e7739b
Lannnnh <15997426361@sina.cn>
2019-09-23 20:39:51 +0800

    add copyable

---
+++ base/copyable.h
@@ -0,0 +1,11 @@
+#ifndef _BASE_COPYABLE_
+#define _BASE_COPYABLE_
+
+class copyable
+{
+    protected:
+        copyable() = default;
+        ~copyable() = default;
+};
+
+#endif // end _BASE_COPYABLE_
