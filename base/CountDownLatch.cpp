f9c73794ac6ad09ad4d4152aeef74fcf24f754b4
Lannnnh <15997426361@sina.cn>
2019-09-26 21:45:35 +0800

    modify CurrentDownLatch

--- base/CountDownLatch.cpp
+++ base/CountDownLatch.cpp
@@ -1,5 +1,15 @@
 #include "CountDownLatch.h"
 
+/*
+    允许一个或者多个线程等待其他线程执行到某一个操作。
+
+    两种典型用法：
+    1. 某一个线程在开始运行之前等待n个线程执行完毕。
+
+    2. 实现多个线程开始执行任务的最大并行性，比如，在初始化一个共享的CountDownLatch对象，count为1，多个线程各自wait（），在主线程countdown（），
+    那么各个线程就一起运行了。
+*/
+
 CountDownLatch::CountDownLatch(int count)
     : mutex_(),
       condition_(mutex_),
--- base/CurrentThread.cpp
+++ base/CurrentThread.cpp
@@ -1,7 +1,1 @@
-#include "CurrentThread.h"
-#include <stdlib.h>
 
-__thread int t_cachedTid = 0;
-__thread char t_tidString[32];
-__thread int t_tidStringLength = 6;
-__thread const char* t_threadName = "unknow";
