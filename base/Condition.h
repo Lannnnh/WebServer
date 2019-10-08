993082f7f35395e021f8dd7dcde12e79aaf31274
Lannnnh <15997426361@sina.cn>
2019-10-08 17:01:44 +0800

    mofify Condition

--- base/Condition.h
+++ base/Condition.h
@@ -20,6 +20,10 @@ class Condition : nocopyable
             pthread_cond_destroy(&cond);
         }
 
+        /*
+            wait中的mutex用于保护条件变量，调用这个函数进行等待的条件发生时，mutex会被自动释放，以供其他线程改变条件
+            wait中的两个步骤是原子性的： 1. 把调用线程放到条件等待队列上。 2. 释放mutex
+        */
         void wait()
         {
             pthread_cond_wait(&cond, mutex.get());
