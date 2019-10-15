5e347c79361b9c5f79fcada0c007230cc93229ff
Lannnnh <15997426361@sina.cn>
2019-10-15 22:04:22 +0800

    modify TimerQueue

--- net/TimerQueue.h
+++ net/TimerQueue.h
@@ -1,6 +1,27 @@
 #ifndef _NET_TIMERQUEUE_H
 #define _NET_TIMERQUEUE_H
 
+#include "base/nocopyable.h"
+#include "TimerId.h"
+#include "Callbacks.h"
+#include "base/Timestamp.h"
+
+class EventLoop;
+
+class TimerQueue : nocopyable
+{
+    public:
+        TimerQueue(EventLoop *loop);
+        ~TimerQueue();
+
+        TimerId addTimer(const TimerCallback &cb,
+                         Timestamp when,
+                         double interval);
+
+    private:
+        EventLoop *loop_;
+        const int timerfd_;
+};
 
 
 
