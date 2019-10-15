f744562e5c2549b563b29a1e5dfb4c0b546870b8
Lannnnh <15997426361@sina.cn>
2019-10-15 22:03:48 +0800

    modify TImer

--- net/Timer.h
+++ net/Timer.h
@@ -2,13 +2,42 @@
 #define _NET_TIMER_H
 
 #include "base/nocopyable.h"
+#include "base/Atomic.h"
+#include "base/Timestamp.h"
+#include "Callbacks.h"
 
 class Timer : nocopyable
 {
     public:
+        Timer(TimerCallback cb, Timestamp when, double interval)
+            : callback_(cb),
+              expiration_(when),
+              interval_(interval),
+              repeat_(interval_ > 0.0),
+              sequence_(s_numCreated_.incrementAndGet())
+    {   }
+
+    void run() const
+    {
+        callback_();
+    }
+
+    Timestamp expiration() const { return expiration_; }
+    bool repeat() const { return repeat_; }
+    int64_t sequence() const { return sequence_; }
+
+    void restart(Timestamp now);
+
+    static int64_t numCreated() { return s_numCreated_.get(); }
 
     private:
-        
+        const TimerCallback callback_;
+        Timestamp expiration_;
+        const double interval_;
+        const bool repeat_;
+        const int64_t sequence_;
+
+        static AtomicInt64 s_numCreated_;
 };
 
 
