7b32da90361ba9923fc01315e7b92f4818af1c9c
Lannnnh <15997426361@sina.cn>
2019-10-25 00:00:17 +0800

    modify TcpConnection

--- net/TcpConnection.h
+++ net/TcpConnection.h
@@ -33,11 +33,62 @@ class TcpConnection : nocopyable, public std::enable_shared_from_this<TcpConnect
         const struct ::sockaddr_in& peerAddr() const { return peerAddr_; }
         bool connected() const { return state_ == kConnected; }
         bool disconnected() const { state_ == kDisconnected; }
-        // return true if success, continue
+        // send
+        void send(const void *messeage, int len);
+        void send(Buffer *message); // this one will swap data
+        void shutdown(); // not thread safe, no simitaneous calling
+        void forceClose();
+        void forceCloseWithDelay(double seconds);
+        void setTcpNoDelay(bool on);
+        // reading or not
+        void startRead();
+        void stopRead();
+        bool isReading() const { return reading_; }
+
+        void setConnectionCallback(const ConnectionCallback &cb)
+        { connectionCallback_ = cb; }
+
+        void setMessageCallback(const MessageCallback &cb)
+        { messageCallback_ = cb; }
+
+        void setWriteCompleteCallback(const WriteCompleteCallback &cb)
+        { writeCompleteCallback_ = cb; }
+
+        void setHighWaterMarkCallback(const HighWaterMarkCallback &cb, size_t highWaterMark)
+        { highWaterMarkCallback_ = cb; highWaterMark_ = highWaterMark; }
+
+        // advanced interface
+        Buffer* inputBuffer()
+        { return &inputBuffer_; }
+
+        Buffer* outputBuffer()
+        { return &outputBuffer_; }
+
+        // internal use only
+        void setCloseCallback(const CloseCallback &cb)
+        { closeCallback_ = cb; }
+
+        // called when TcpServer accepts a new connection
+        void connectEstablished(); // should be called only once
+        // called when TcpServer has removed me from its map
+        void connectDestroyed(); // should be called only once
+
+
     private:
         enum stateE { kDisconnected, kConnecting, kConnected, kDisconnecting };
+        void handleRead(Timestamp receiveTime);
+        void handleWrite();
+        void handleClose();
+        void handleError();
+
+        void sendInLoop(const void *message, size_t len);
+        void shutdownInLoop();
+        void forceCloseInLoop();
 
         void setState(stateE s) {}
+        const char* stateToString() const;
+        void startReadInLoop();
+        void stopReadInLoop();
 
         EventLoop *loop_;
         std::string name_;
