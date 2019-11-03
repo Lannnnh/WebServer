6c9f34c6089e74ff5c213bff2aa64ac31bd29099
Lannnnh <15997426361@sina.cn>
2019-11-04 00:41:25 +0800

    modify Tcpserver

--- net/TcpServer.cpp
+++ net/TcpServer.cpp
@@ -1,6 +1,41 @@
 #include "TcpServer.h"
 #include "EventLoop.h"
 #include "Acceptor.h"
+#include "Socket.h"
+#include "EventLoopThreadPool.h"
+
+#include <stdio.h>
+
+TcpServer::TcpServer(EventLoop *loop,
+                     const ::sockaddr_in &listenAddr,
+                     const std::string &name,
+                     Option option)
+    : loop_(loop),
+      ipPort_(sockets::toIpPort(&listenAddr)),
+      name_(name),
+      acceptor_(new Acceptor(loop, &listenAddr, option == kReusePort)),
+      threadPool_(new EventLoopThreadPool(loop, name_)),
+      connectionCallback_(defaultConnectionCallback),
+      messageCallback_(defaultMessageCallback),
+      nextConnId_(1)
+{   
+    acceptor_->setNewConnectionCallback(
+        std::bind(&TcpServer::newConnection, this, _1, _2));
+}
+
+TcpServer::~TcpServer()
+{
+    loop_->assertInLoopThread();
+    // LOG_TRACE << "TcpServer::~TcpServer [" << name_ << "] destructing";
+
+    for (auto &item : connections_)
+    {
+        TcpConnectionPtr conn(item.second);
+        item.second.reset();
+        conn->getLoop()->runInLoop(
+            std::bind(&TcpConnection::connectDestroyed, conn));
+    }
+}
 
 
 
