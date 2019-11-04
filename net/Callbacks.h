15037aa6c9d9a95bff16ade575110fa743e8549c
Lannnnh <15997426361@sina.cn>
2019-11-04 22:07:52 +0800

    modify Callback

--- net/Callbacks.h
+++ net/Callbacks.h
@@ -22,22 +22,22 @@ inline T* get_pointer(const std::unique_ptr<T> &ptr)
     return ptr->get();
 }
 
-    class Buffer;
-    class TcpConnection;
-    typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
-    typedef std::function<void ()> TimerCallback;
-    typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
-    typedef std::function<void (const TcpConnectionPtr&)> CloseCallback;
-    typedef std::function<void (const TcpConnectionPtr&)> WriteCompleteCallback;
-    typedef std::function<void (const TcpConnectionPtr&, size_t)> HighWaterMarkCallback;
+class Buffer;
+class TcpConnection;
+typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
+typedef std::function<void ()> TimerCallback;
+typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
+typedef std::function<void (const TcpConnectionPtr&)> CloseCallback;
+typedef std::function<void (const TcpConnectionPtr&)> WriteCompleteCallback;
+typedef std::function<void (const TcpConnectionPtr&, size_t)> HighWaterMarkCallback;
 
-    typedef std::function<void (const TcpConnectionPtr&,
-                                Buffer*,
-                                Timestamp)> MessageCallback;
+typedef std::function<void (const TcpConnectionPtr&,
+                            Buffer*,
+                            Timestamp)> MessageCallback;
 
-    void defaultConnectionCallback(const TcpConnectionPtr& conn);
-    void defaultMessageCallback(const TcpConnectionPtr& conn,
-                                Buffer* buffer,
-                                Timestamp receiveTime);
+void defaultConnectionCallback(const TcpConnectionPtr& conn);
+void defaultMessageCallback(const TcpConnectionPtr& conn,
+                            Buffer* buffer,
+                            Timestamp receiveTime);
 
 #endif // end _NET_CALLBACKS_H
