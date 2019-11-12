99aea374a6b6dbda68d39580187c084a057efa02
Lannnnh <15997426361@sina.cn>
2019-11-12 22:11:20 +0800

    modify LogStream

---
+++ base/LogStream.h
@@ -0,0 +1,14 @@
+#ifndef _BASE_LOGSTREAM_H
+#define _BASE_LOGSTREAM_H
+
+#include "nocopyable.h"
+
+#include <assert.h>
+#include <string.h>
+
+class AsyncLogging;
+const int kSmallBuffer = 4000;
+const int kLargeBuffer = 4000 * 1000;
+
+
+#endif // end _BASE_LOGSTREAM_H
