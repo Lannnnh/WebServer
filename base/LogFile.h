88838580de1c7807359e98deb74550dacc9aef22
Lannnnh <15997426361@sina.cn>
2019-11-12 22:10:56 +0800

    modify Logfile

---
+++ base/LogFile.h
@@ -0,0 +1,36 @@
+#ifndef _BASE_LOGFILE_H
+#define _BASE_LOGFILE_H
+
+#include "MutexLock.h"
+#include "type.h"
+#include "nocopyable.h"
+
+#include <memory>
+
+class AppendFile;
+
+class LogFile : nocopyable
+{
+    public:
+        LogFile(const std::string& basename, int flushEveryN = 1024);
+        ~LogFile();
+
+        void append(const char* logline, int len);
+        void flush();
+        bool rollFile();
+
+
+    private:
+        void append_unlocked(const char* logline, int len);
+
+        const std::string basename_;
+        const int flushEvertN_;
+
+        int count_;
+        std::unique_ptr<MutexLock> mutex_;
+        std::unique_ptr<AppendFile> file_;
+};
+
+
+
+#endif // end _BASE_LOGFILE_H
