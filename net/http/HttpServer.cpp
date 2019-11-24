93433ebea2b5b8333d40e24bd50717a6c07ebb5b
Lannnnh <15997426361@sina.cn>
2019-11-25 00:49:02 +0800

    modify HttpServer

--- net/http/HttpServer.cpp
+++ net/http/HttpServer.cpp
@@ -31,4 +31,46 @@ void HttpServer::start()
     server_.start();
 }
 
-// need continue
+void HttpServer::onConnection(const TcpConnectionPtr& conn)
+{
+    if (conn->connected())
+    {
+        conn->setContext(HttpContext());
+    }
+}
+
+void HttpServer::onMessage(const TcpConnectionPtr& conn,
+                           Buffer* buf,
+                           Timestamp receiveTime)
+
+{
+    HttpContext* context = boost::any_cast<HttpContext> (conn->getMutableContext());
+
+    if (!context->parseRequest(buf, receiveTime))
+    {
+        conn->send("HTTP/1.1 400 Bad Request\r\n\r\n");
+        conn->shutdown();
+    }
+
+    if (context->gotAll())
+    {
+        onRequest(conn, context->request());
+        context->reset();
+    }
+}
+
+void HttpServer::onRequest(const TcpConnectionPtr& conn, const HttpRequest& req)
+{
+    const std::string& connection = req.getHeader("Connection");
+    bool close = connection == "close" ||
+                 (req.getVersion() == HttpRequest::kHttp10 && connection != "Keep-Alive");
+    HttpResponse response(close);
+    httpCallback_(req, &response);
+    Buffer buf;
+    response.appendToBuffer(&buf);
+    conn->send(&buf);
+    if (response.closeConnection())
+    {
+        conn->shutdown();
+    }
+}
