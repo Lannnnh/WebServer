6775086b48910a20cf66f96dfcaf29b139bb9185
Lannnnh <15997426361@sina.cn>
2020-01-11 21:04:24 +0800

    add Epoller

---
+++ WebServer/net/Epoller.cpp
@@ -0,0 +1,79 @@
+#include "WebServer/net/Epoller.h"
+#include "WebServer/net/Channel.h"
+#include "WebServer/base/Logging.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <poll.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+const int kNew = -1;
+const int kAdded = 1;
+const int kDeleted = 2;
+
+Epoller::Epoller(EventLoop* loop) 
+    : ownerLoop_(loop),
+      epollfd_(::epoll_create1(EPOLL_CLOEXEC)),
+      events_(KInitEventListSize)
+{
+    if (epollfd_ < 0)
+    {
+        LOG << "Epoller::Epoller()";
+    }
+}
+
+Epoller::~Epoller()
+{
+    ::close(epollfd_);
+}
+
+Timestamp Epoller::poll(int timeoutMs, ChannelList* activeChannels)
+{
+    LOG << "fd total count " << channels_.size();
+    int numEvents = ::epoll_wait(epollfd_,
+                                 &*events_.begin(),
+                                 events_.size(),
+                                 timeoutMs);
+
+    int savedErrno = errno;
+    Timestamp now(Timestamp::now());
+    if (numEvents > 0)
+    {
+        LOG << numEvents << " events happened";
+        fillActiveChannels(numEvents, activeChannels);
+        if (numEvents == events_.size())
+        {
+            events_.resize(events_.size()*2);
+        }
+    }
+    else if (numEvents == 0)
+    {
+        LOG << "Nothing happened";
+    }
+    else
+    {
+        if (savedErrno != EINTR)
+        {
+            errno = savedErrno;
+            LOG << "Epoller::poll()";
+        }
+    }
+    return now;
+}
+
+void Epoller::fillActiveChannels(int numEvents, ChannelList* activeChannels) const
+{
+    assert(numEvents <= events_.size());
+    for (int i = 0; i < numEvents; ++ i )
+    {
+        Channel* channel = static_cast<Channel*> (events_[i].data.ptr);
+        int fd = channel->fd();
+        ChannelMap::const_iterator it = channels_.find(fd);
+        assert(it != channels_.end());
+        assert(it->second == channel);
+
+        channel->set_revents(events_[i].events);
+        activeChannels->push_back(channel);
+    }
+}
---
+++ WebServer/net/Epoller.h
@@ -0,0 +1,50 @@
+#ifndef _NET_EPOLLER_H
+#define _NET_EPOLLER_H
+
+#include "WebServer/base/nocopyable.h"
+#include "WebServer/base/Timestamp.h"
+
+#include <functional>
+#include <map>
+#include <vector>
+
+struct epoll_event;
+class Channel;
+class EventLoop;
+
+class Epoller : nocopyable
+{
+    public:
+        typedef std::vector<Channel*> ChannelList;
+
+        Epoller(EventLoop* loop);
+        ~Epoller();
+        
+        Timestamp poll(int timeoutMs, ChannelList* activeChannels);
+
+        void updateChannel(Channel* channel);
+        void removeChannel(Channel* channel);
+        bool hasChannel(Channel* channel);
+        void assertInLoopThread() const;
+        static Epoller* newDefaultPoller(EventLoop* loop);
+
+    protected:
+        typedef std::map<int, Channel*> ChannelMap;
+        ChannelMap channels_;
+
+    private:
+        static const int KInitEventListSize = 16;
+
+        static const char* operationToString(int op);
+
+        void fillActiveChannels(int numEvents, ChannelList* activeChannels) const;
+        void update(int operation, Channel* channel);
+
+        typedef std::vector<struct epoll_event> EventList;
+
+        int epollfd_;
+        EventList events_;
+        EventLoop* ownerLoop_;
+};
+
+#endif // end _NET_EPOLLER_H
