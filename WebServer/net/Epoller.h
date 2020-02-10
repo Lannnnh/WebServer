d564efd463162f2e92e2fd7c0055e06781d3b6de
Lannnnh <15997426361@sina.cn>
2020-02-10 21:51:46 +0800

    modify Epoller

--- WebServer/net/Epoller.h
+++ WebServer/net/Epoller.h
@@ -1,14 +1,9 @@
 #ifndef _NET_EPOLLER_H
 #define _NET_EPOLLER_H
 
-<<<<<<< HEAD
 #include "nocopyable.h"
 #include "Timestamp.h"
-=======
-#include "WebServer/base/nocopyable.h"
-#include "WebServer/base/Timestamp.h"
-#include "WebServer/net/EventLoop.h"
->>>>>>> 1683a00cadccbba4d0f2bf08172d11c643f84afa
+#include "EventLoop.h"
 
 #include <functional>
 #include <map>
